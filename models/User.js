844b338ad4bd78a960ac5c872d13260bf9a65c6b
hariaravind03 <142722524+hariaravind03@users.noreply.github.com>
2024-10-13 21:37:10 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -10,10 +10,10 @@ const userSchema = new mongoose.Schema({
         type: String, 
         required: true 
     },
-    // You can add more fields as needed, e.g., email, profile picture, etc.
+
 });
 
-// Create the User model
+
 const User = mongoose.model('User', userSchema);
 
-module.exports = User; // Export the User model
+module.exports = User; 
