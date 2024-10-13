1229f90a4696ce12e93ddcdeb1e8518ea7635d3e
hariaravind03 <142722524+hariaravind03@users.noreply.github.com>
2024-10-13 21:36:48 +0530

    Update Resume.js

--- models/Resume.js
+++ models/Resume.js
@@ -1,20 +1,21 @@
 const mongoose = require('mongoose');
 
-// Schema for education records
+
 const experienceSchema = new mongoose.Schema({
-    year: { type: String, required: true },      // Year of experience
-    company: { type: String, required: true },    // Company name
-    designation: { type: String, required: true }, // Job title
-    description: { type: String, required: false } // Optional description
-}, { _id: false }); // Disable automatic ID generation for subdocuments
-// Main resume schema
+    year: { type: String, required: true }, 
+    company: { type: String, required: true },    
+    designation: { type: String, required: true }, 
+    description: { type: String, required: false } 
+}, { _id: false });
+
+
 const resumeSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
     name: String,
     mobile: String,
     email: String,
     summary: String,
-    education: String, // Change education to an array of education objects
+    education: String, 
     skills: String,
     certifications: String,
     additionalContent: String,
@@ -22,6 +23,5 @@ const resumeSchema = new mongoose.Schema({
     experience: [experienceSchema],
 });
 
-// Export the model
 module.exports = mongoose.model('Resume', resumeSchema);
 
